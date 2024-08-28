f5a115035b7210ba360a2125e89c61bb0ec4a533
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-28 15:00:39 +0530

    mysql-shell

--- backend.sh
+++ backend.sh
@@ -51,34 +51,3 @@ then
 else 
     echo -e "User expense already exists $Y skipping $N"
 fi
-
-mkdir -p /app
-VALIDATE $? "creating /app folder"
-
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-VALIDATE $? "downloading backend application"
-
-cd /app
-rm -rf /app/* # remove the existing code 
-unzip /tmp/backend.zip &>>$LOG_FILE
-VALIDATE $? "unzipping/extracting backend application"
-
-npm install &>>$LOG_FILE
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
-
-# load the data before running backend 
-
-dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Install mysql"
-
-mysql -h mysql.daws81s.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-VALIDATE $? "loading schema"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "reload daemon"
-
-systemctl enable backend &>>$LOG_FILE
-VALIDATE $? "enable backend service"
-
-systemctl restart backend &>>$LOG_FILE
-VALIDATE $? "restart backend service"
